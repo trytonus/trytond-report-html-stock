846d76ab8168d4c0886e6dbbe653de4317ea2e23
Prakash Pandey <prakash.pandey@fulfil.io>
2015-07-28 20:32:04 +0530

    Added consolidated picking list
    Select multiple shipments and the consolidated report will show
    lines to pick grouped by the product and the location.

--- report_html_stock.py
+++ report_html_stock.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+from itertools import groupby, imap, chain
+
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
 from openlabs_report_webkit import ReportWebkit
 
 __all__ = [
-    'PickingList', 'SupplierRestockingList', 'CustomerReturnRestockingList'
+    'PickingList', 'SupplierRestockingList', 'CustomerReturnRestockingList',
+    'ConsolidatedPickingList'
 ]
 __metaclass__ = PoolMeta
 
@@ -77,6 +80,64 @@ class PickingList(ReportMixin):
             move.from_location.rec_name, move.product.name
         )
         return super(PickingList, cls).parse(
+            report, records, data, localcontext
+        )
+
+
+class ConsolidatedPickingList(ReportMixin):
+    """
+    Consolidated Picking List.
+    """
+    __name__ = 'report.consolidated_picking_list'
+
+    @classmethod
+    def group_key(cls, move):
+        """
+        Key function for grouping and sorting of
+        moves
+        """
+        return (move.from_location, move.product)
+
+    @classmethod
+    def get_product_repr_from(cls, key):
+        """
+        Returns the product representation from the key
+        """
+        return key[1].rec_name
+
+    @classmethod
+    def get_location_repr_from(cls, key):
+        """
+        Returns the location representation from the key
+        """
+        return key[0].rec_name
+
+    @classmethod
+    def parse(cls, report, records, data, localcontext):
+        """
+        The default implementation groups by product
+        and sorts by from_location.
+        """
+        localcontext['grouped_moves'] = []
+        for key, grouper in groupby(
+                # Sort all the moves from all shipments
+                # and then group it
+                list(sorted(
+                    # Chain all inventory moves from all shipments
+                    chain(*imap(lambda s:s.inventory_moves, records)),
+                    key=cls.group_key
+                )), cls.group_key
+            ):
+            moves = list(grouper)
+            # TODO: Sum totals everything, the UOM to base UOM conversion
+            # is not done
+            localcontext['grouped_moves'].append(
+                (key, moves, sum(map(lambda m:m.quantity, moves)))
+            )
+
+        localcontext['get_product_repr_from'] = cls.get_product_repr_from
+        localcontext['get_location_repr_from'] = cls.get_location_repr_from
+        return super(ConsolidatedPickingList, cls).parse(
             report, records, data, localcontext
         )
 
